b6a37cfff69ce8f7978f2b2e9e518cf3c31d4860
rosso101 <victorprivv101@gmail.com>
2023-10-29 15:19:16 +0300

    completed assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,27 @@
-import React from "react";
+// import React from "react";
 
-function TaskList() {
+// function TaskList() {
+//   return (
+//     <div className="tasks">
+//       {/* display a list of tasks using Task component */}
+//     </div>
+//   );
+// }
+
+// export default TaskList;
+
+import React from "react";
+import Task from "./Task";
+
+const TaskList = ({ tasks }) => {
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
+    <ul>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </ul>
   );
-}
+};
 
 export default TaskList;
+
